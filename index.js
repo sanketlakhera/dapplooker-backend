7a855454b6091e4f749d09686ae45e011c3a44cd
Sanket <lakhera.sanket850@gmail.com>
2026-03-13 00:25:49 +0530

    fix: port updated as per env

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const swaggerSpec = swaggerJsdoc({
 });
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
